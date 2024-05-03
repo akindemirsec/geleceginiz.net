6118777a46bd4996ca9052171b205d5ff67c662e
Akın Demir <akindemirsec@gmail.com>
2024-05-03 05:28:56 +0300

    fixed issues

--- core/admin.py
+++ core/admin.py
@@ -1,12 +1,24 @@
-# admin.py dosyası
 from django.contrib import admin
 from .models import *
 
 class GnUserAdmin(admin.ModelAdmin):
-    list_display = ['userid', 'name', 'surname', 'mail', 'username']  # Liste görünümünde hangi alanların görüntüleneceğini belirtin
+    list_display = ['userid', 'name', 'surname', 'mail', 'username']  
 
+class GnFakulteAdmin(admin.ModelAdmin):
+    list_display = ['fakulte_id', 'fakulte_name', 'fakulte_uni']  
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['course_id', 'name', 'bolum', 'description', 'video_url']  
+
+class GnBolumAdmin(admin.ModelAdmin):
+    list_display = ['bolum_id', 'bolum_name', 'bolum_fakulte']  
+
+class GnUniversiteAdmin(admin.ModelAdmin):
+    list_display = ('uni_name', 'uni_city', 'uni_logo')
+
+# Modeller için admin sınıflarını kaydet
 admin.site.register(Gn_user, GnUserAdmin)
-admin.site.register(Gn_universite)
-admin.site.register(Gn_Fakulte)
-admin.site.register(Course)
-admin.site.register(Gn_Bolum)
+admin.site.register(Gn_Fakulte, GnFakulteAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Gn_Bolum, GnBolumAdmin)
+admin.site.register(Gn_universite, GnUniversiteAdmin)
--- core/models.py
+++ core/models.py
@@ -9,10 +9,16 @@ class Gn_user(models.Model):
     password = models.CharField(max_length=64)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
 class Gn_admin(models.Model):
     adminid = models.AutoField(primary_key=True) 
     username = models.CharField(max_length=16)
     password = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.username
 
 class Gn_universite(models.Model):
     uni_id = models.AutoField(primary_key=True)
@@ -20,15 +26,25 @@ class Gn_universite(models.Model):
     uni_city = models.CharField(max_length=100)
     uni_logo = models.ImageField(upload_to='uni_logo/', blank=True, null=True)
 
+    def __str__(self):
+        return self.uni_name
+
 class Gn_Fakulte(models.Model):
     fakulte_id = models.AutoField(primary_key=True)
     fakulte_name = models.CharField(max_length=64)
     fakulte_uni = models.ForeignKey(Gn_universite, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.fakulte_name
+
 class Gn_Bolum(models.Model):
     bolum_id = models.AutoField(primary_key=True)
     bolum_name = models.CharField(max_length=100)
     bolum_fakulte = models.ForeignKey(Gn_Fakulte, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.bolum_name} - {self.bolum_fakulte.fakulte_uni.uni_name}"
+
 
 class Course(models.Model):
     course_id = models.AutoField(primary_key=True)
@@ -36,3 +52,6 @@ class Course(models.Model):
     bolum = models.ForeignKey(Gn_Bolum, on_delete=models.CASCADE)
     description = models.TextField()
     video_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
