d308fea193e49aa614ead5ab6a9d5215fb81abdf
GrimezSec <akindemirsec@gmail.com>
2024-05-03 15:37:11 +0300

    fixed some issues #todo: fix universite views

--- Universiteler/urls.py
+++ Universiteler/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.universiteler, name="universiteler"),
+    path('universiteler/<int:uni_id>/', views.uni_fakulteler, name='uni_fakulteler'),
+    path('fakulteler/<int:fakulte_id>/courses/', views.fakulte_bolumler, name='fakulte_courses'),
+    path('course/<int:course_id>/', views.course_detail, name='course_detail'),
 ]
--- Universiteler/views.py
+++ Universiteler/views.py
@@ -1,13 +1,60 @@
-from django.shortcuts import render
-from core.models import Gn_universite, Gn_user
+from django.shortcuts import get_object_or_404, render
+from core.models import Gn_universite, Gn_user, Gn_Fakulte, Gn_Bolum, Course
 
 def universiteler(request):
     user_id = request.session.get('user_id')
     user = None
     if user_id:
         try:
-            user = Gn_user.objects.get(userid=user_id)  # Burada user_id yerine id kullanmalısınız.
+            user = Gn_user.objects.get(userid=user_id) 
         except Gn_user.DoesNotExist:
             pass
     universities = Gn_universite.objects.all()
     return render(request, 'universiteler.html', {'universities': universities, "user": user})
+
+def uni_fakulteler(request, uni_id):
+    user_id = request.session.get('user_id')
+    user = None
+    if user_id:
+        try:
+            user = Gn_user.objects.get(userid=user_id) 
+        except Gn_user.DoesNotExist:
+            pass
+    universite = get_object_or_404(Gn_universite, pk=uni_id)
+    fakulteler = Gn_Fakulte.objects.filter(fakulte_uni=universite)
+    return render(request, 'uni_fakulteler.html', {'universite': universite, 'fakulteler': fakulteler, "user": user})
+
+def fakulte_bolumler(request, fakulte_id):
+    user_id = request.session.get('user_id')
+    user = None
+    if user_id:
+        try:
+            user = Gn_user.objects.get(userid=user_id) 
+        except Gn_user.DoesNotExist:
+            pass
+    fakulte = get_object_or_404(Gn_Fakulte, pk=fakulte_id)
+    bolumler = Gn_Bolum.objects.filter(bolum_fakulte=fakulte)
+    return render(request, 'fakulte_bolumler.html', {'fakulte': fakulte, 'bolumler': bolumler, "user": user})
+
+def bolum_courses(request, bolum_id):
+    user_id = request.session.get('user_id')
+    user = None
+    if user_id:
+        try:
+            user = Gn_user.objects.get(userid=user_id) 
+        except Gn_user.DoesNotExist:
+            pass
+    bolum = get_object_or_404(Gn_Bolum, pk=bolum_id)
+    courses = Course.objects.filter(bolum=bolum)
+    return render(request, 'bolum_courses.html', {'bolum': bolum, 'courses': courses, "user": user})
+
+def course_detail(request, course_id):
+    user_id = request.session.get('user_id')
+    user = None
+    if user_id:
+        try:
+            user = Gn_user.objects.get(userid=user_id) 
+        except Gn_user.DoesNotExist:
+            pass
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'course_detail.html', {'course': course, "user": user})
